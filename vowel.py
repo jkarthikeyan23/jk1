e517b8db7bd858c8806a7076f72da5eb299a220e
jkarthikeyan23 <49342124+jkarthikeyan23@users.noreply.github.com>
2019-04-30 20:20:56 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,8 @@
+ch=int(input("enter a character"))
+if(ch==a,e,i,o,u):
+print("the letter is vowel")
+elif(ch==Z,T,B,G,H);
+print("the letter is a consonant")
+else:
+print("the letter is invaild")
+
