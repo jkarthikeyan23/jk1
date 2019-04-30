73de6b371a0c6a1c1d64fc5f63eafba289c1d0a3
jkarthikeyan23 <49342124+jkarthikeyan23@users.noreply.github.com>
2019-04-30 09:37:44 +0530

    largest.c

---
+++ largest.c
@@ -0,0 +1,19 @@
+#include<studio.h>
+int main()
+{
+ int num1,num2,num3;
+ printf("enter three numbers\n")
+ scanf("%d %d",&num1,&num2,&num3);
+ if(num1>num2)&&(num1>num2))
+ {
+ printf("%d",num1);
+ else if(num2>num1)&& (num2>num3);
+ { 
+ printf("%d",num2);
+ }
+ else
+ {
+ printf("%d",num3);
+ }
+ return 0;
+ }
