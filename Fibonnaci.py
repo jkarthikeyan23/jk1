00bc3b073a0bf58dffd1734d0100d22e77214c40
jkarthikeyan23 <49342124+jkarthikeyan23@users.noreply.github.com>
2019-05-01 18:18:15 +0530

    Create Fibonnaci.py

---
+++ Fibonnaci.py
@@ -0,0 +1,14 @@
+nterms=int(input("enter a number"))
+n1=0
+n2=1
+count=0
+if nterms<=0:
+print("please enter a positive integer")
+elif nterms==1
+print("fibnacci sequence upto",nterms,":")
+print(n1)
+else:
+print(n1,end=',')
+nth=n1+n
+n2=nth
+count+=1
