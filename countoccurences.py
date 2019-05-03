bb3e28b04baf6b4043b7175b92548745a6df9f76
jkarthikeyan23 <49342124+jkarthikeyan23@users.noreply.github.com>
2019-05-03 12:17:22 +0530

    Create countoccurences.py

---
+++ countoccurences.py
@@ -0,0 +1,14 @@
+def countoccurrences(e,d):
+n=len(s)
+c1=0
+c2=0
+c=0
+for i in range(n):
+if e[i]=='a':
+c1+=1
+ife[i]=='b':
+c+=c1
+return c*d+(d-1)/2)*c1c2
+e="abcd"
+d=2
+print(countoccurrences(s,k))
