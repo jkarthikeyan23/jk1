b32958c0d2e3ac76872256b2ade1d062bfcba2b0
jkarthikeyan23 <49342124+jkarthikeyan23@users.noreply.github.com>
2019-05-03 12:25:06 +0530

    Create Comparetwostrings.py

---
+++ Comparetwostrings.py
@@ -0,0 +1,12 @@
+print("enter 'q' for exit.")
+string1=input("enter first string")
+if string1=='q':
+exit():
+else:
+string2=input("enter second string:")
+if string1==string2:
+print("\nboth string are equal to each other.")
+print(string1,"==",string2);
+else:
+print("\nstrings are not equal.")
+print(string1."!=",string2);
