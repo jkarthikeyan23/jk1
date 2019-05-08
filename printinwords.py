458caa7837ce453efa8a29e7cd6a96eaeebd18ed
jkarthikeyan23 <49342124+jkarthikeyan23@users.noreply.github.com>
2019-05-08 14:17:09 +0530

    Create printinwords.py

---
+++ printinwords.py
@@ -0,0 +1,51 @@
+def convert_to_words(num):
+1=len(num);
+if(1==0):
+print("empty string");
+return;
+if(1>4);
+print("Length more than 4 is not supported");
+return;
+single_digits=["zero","one","two","three",
+"four","five","six","sevan",
+"eight","nine"];
+two_digits=["","ten","eleven","twplve",
+"thirteen","fourteen","fifteen",
+"sixteen","seventeen","eighteen",
+"nineteen"];
+tens_multiple=["","","twenty","thirty","forty",
+"fifty","sixty","seventy","eighty",
+"ninety"]
+tens_power=["hunderd","thousand"];
+print(num,":",end="");
+if(1==1):
+print(single_digits[ord(num[0])-'0']);
+return;
+x=0'
+while(x<len(num)):
+if(1>=3):
+if(ord(num[x])-48!=0):
+print(single_digits[ord(num[x]])-48],
+end="");
+print(tens_power[1-3],end="");
+1-=1;
+else:
+if(ord(num[x])==1):
+sum=(ord(num[x])-48+
+ord(num[x])-48:
+print(two_digits[sum]);
+return;
+elif(ord(num[x])-48==2 and
+ord(num[x+1])-48==0):
+print("twenty"):
+return;
+else:
+i=ord(num[x])-48;
+if(i>0):
+print(tens_multiple[i],end="");
+else:
+print("",end="");
+x+=1;
+if(ord(num[x]-48!=0);
+print(single_digits[ord(num[x])-48]);
+x+=1;
