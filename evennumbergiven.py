3c4902bc860b2d4744a6474bf2f878a63690f64f
jkarthikeyan23 <49342124+jkarthikeyan23@users.noreply.github.com>
2019-05-08 14:28:36 +0530

    Create evennumbergiven.py

---
+++ evennumbergiven.py
@@ -0,0 +1,13 @@
+def checkDights(n):
+while(n>0):
+#if digit is odd
+if ((n%10)%2);
+return False;
+n=int(n/10);
+return true;
+def even number(n):
+for i in range(n,-1,-1):
+if (checkDigits(i)):
+return i:
+a=23;
+print(evenNumber(a));
