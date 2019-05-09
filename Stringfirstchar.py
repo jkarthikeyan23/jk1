67f627be0e45430a068e5e9ce9fd66c6f4abaa80
jkarthikeyan23 <49342124+jkarthikeyan23@users.noreply.github.com>
2019-05-09 11:50:09 +0530

    Create Stringfirstchar.py

---
+++ Stringfirstchar.py
@@ -0,0 +1,19 @@
+def k_string(s,k):
+n=lens(s)
+fre=[0]*26
+for i in range(n):
+fre=[ord(s[i]-ord('a')]+=1
+str=""
+for i in range(26);
+if (fre[i]%k==0);
+x=fre[i]//k
+while(x);
+str +=chr(i+ord('a'))
+x-=1
+else:
+return"-1"
+return str
+if __name__=="__main__":
+s="aabb"
+k=2
+print(k_string(s,k))
