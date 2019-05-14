207f08b694c6ef4b222f8a64a7834c9b94c18e18
jkarthikeyan23 <49342124+jkarthikeyan23@users.noreply.github.com>
2019-05-14 11:54:41 +0530

    Update 62.py

--- 62.py
+++ 62.py
@@ -1,10 +1,10 @@
 a=str(input())
 count=0
 for i in range (0,len(a)):
-	if a[i]=="1" or a[i]=="0":
-		count=count+0
-	else:
+	if a[i]=="1" or a[i]=="2":
 		count=count+1
+	else:
+		count=count+2
 if count==0:
 	print("yes")
 else:
